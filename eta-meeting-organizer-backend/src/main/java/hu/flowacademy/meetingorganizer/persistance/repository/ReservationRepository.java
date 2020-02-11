9bbcaff7abb5252dd02e2c4f8377899fb5bfc01c
Peter Horvath <hpeti1988@gmail.com>
2020-02-11 16:39:36 +0100

    Created reservation repository in Repository package

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistance/repository/ReservationRepository.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistance/repository/ReservationRepository.java
@@ -1,4 +1,9 @@
 package hu.flowacademy.meetingorganizer.persistance.repository;
-@
-public interface ReservationRepository {
+
+import hu.flowacademy.meetingorganizer.persistance.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
