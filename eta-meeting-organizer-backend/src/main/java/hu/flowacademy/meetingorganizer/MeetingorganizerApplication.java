2ffc35a4497125d66982665609cb69c30b4ecda2
Robert Boros <robbie13.boros@gmail.com>
2020-02-11 09:13:01 +0100

    init backend

---
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/MeetingorganizerApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.meetingorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingorganizerApplication.class, args);
+	}
+
+}
---
+++ eta-meeting-organizer-backend/src/test/java/hu/flowacademy/meetingorganizer/MeetingorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.meetingorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
