e83e47c512d7ae31c5cd76e3ef01f7149d4b3a09
istiFlow <56822076+istiFlow@users.noreply.github.com>
2020-02-11 21:34:43 +0100

    I50 building model (#53)
    * Created a building model
    
    * created a building model
    
    * Building repository has been created.
    
    * building service has been created.
    
    * building resource has been created
    
    * remove spaces and unnecessary empty lines
    
    * I removed unnecessary spaces and lines.
    
    * added one extra line at the end of the code.

---
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistance/model/Building.java
@@ -0,0 +1,27 @@
+package hu.flowacademy.meetingorganizer.persistance.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "buildings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Building {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+
+    private String address;
+
+    @OneToMany
+    private List<MeetingRoom> meetingRoom;
+}
