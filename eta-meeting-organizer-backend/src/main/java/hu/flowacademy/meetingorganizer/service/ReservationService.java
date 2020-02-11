edba13e102009243e3872fb9b29b4fb1d82227ce
Peter Horvath <hpeti1988@gmail.com>
2020-02-11 17:03:31 +0100

    Created reservation service in service package

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/service/ReservationService.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/service/ReservationService.java
@@ -1,4 +1,46 @@
 package hu.flowacademy.meetingorganizer.service;
 
+import hu.flowacademy.meetingorganizer.persistance.model.Reservation;
+import hu.flowacademy.meetingorganizer.persistance.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
 public class ReservationService {
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    public List<Reservation> findAllReservations () {
+        return reservationRepository.findAll();
+    }
+
+    public Reservation findOneReservationById (Long id) {
+        return reservationRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public ResponseEntity<?> createReservation (Reservation reservation) {
+        reservationRepository.save(reservation);
+        return new ResponseEntity<>(reservation, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Void> deleteReservation (Long id) {
+        reservationRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    public ResponseEntity<Reservation> updateReservation (Long id, Reservation reservation) {
+        if (reservationRepository.findById(id).isPresent()) {
+            Reservation res = reservationRepository.findById(id).get();
+            reservationRepository.save(res);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
 }
