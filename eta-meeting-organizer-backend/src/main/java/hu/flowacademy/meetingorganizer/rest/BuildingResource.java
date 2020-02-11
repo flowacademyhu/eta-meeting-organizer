0b5d97f11e4c3d4879c9cca2a60f00eb51b5e581
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-11 18:31:22 +0100

    building resource has been created

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/BuildingResource.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/BuildingResource.java
@@ -1,4 +1,53 @@
 package hu.flowacademy.meetingorganizer.rest;
 
+import hu.flowacademy.meetingorganizer.persistance.model.Building;
+import hu.flowacademy.meetingorganizer.service.BuildingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
 public class BuildingResource {
+
+    private BuildingService buildingService;
+
+    @PostMapping("/buildings")
+    public ResponseEntity<?> create (@RequestBody Building building){
+        buildingService.createBuilding(building);
+        return new ResponseEntity<>(building, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/buildings")
+    public ResponseEntity<List<Building>> findAll (){
+        List<Building> buildings = buildingService.findAllBuilding();
+        return new ResponseEntity<>(buildings, HttpStatus.OK);
+    }
+
+    @GetMapping("/buildings/{id}")
+    public ResponseEntity<?> findOne (@PathVariable Long id){
+        Building building = buildingService.findBuildingById(id);
+        if(building == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(building, HttpStatus.OK);
+        }
+    }
+
+    @PutMapping("/buildings/{id}")
+    public ResponseEntity<Building> update (@PathVariable Long id, @RequestBody Building building){
+        buildingService.updateBuilding(id, building);
+        return  ResponseEntity.accepted().build();
+    }
+
+    @DeleteMapping("/buildings/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+        buildingService.deleteBuilding(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
