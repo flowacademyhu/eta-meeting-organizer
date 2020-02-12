cf1bae71980cfd7f55d8325d555916709b1de63f
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-12 21:39:05 +0100

    moved to persistence package

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Reservation.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Reservation.java
@@ -1,4 +1,4 @@
-package hu.flowacademy.meetingorganizer.persistance.model;
+package hu.flowacademy.meetingorganizer.persistence.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
