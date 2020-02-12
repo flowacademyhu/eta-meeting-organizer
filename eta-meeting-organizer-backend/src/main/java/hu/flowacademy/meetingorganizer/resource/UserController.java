cbb21c0de3694b7792b28a867a2ed2136d07dcd0
Berente Péter <40863181+berentepeter@users.noreply.github.com>
2020-02-12 10:48:59 +0100

    I41 user endpoint (#54)
    * User model and UserRepository calles
    
    * userservice
    
    * user controller
    
    * deleted an irrelevant line
    
    * refactoring
    
    * UserService class refactored
    
    * UserService: deleteById simplified
    
    * @Transactional added

---
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/resource/UserController.java
@@ -0,0 +1,42 @@
+package hu.flowacademy.meetingorganizer.resource;
+
+import hu.flowacademy.meetingorganizer.persistence.model.User;
+import hu.flowacademy.meetingorganizer.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> userList = userService.findAllUser();
+        return new ResponseEntity<List<User>>(userList, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User userEntity = userService.findUserById(id);
+        return new ResponseEntity<User>(userEntity, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(User user) {
+        User updatedUser = userService.createUser(user);
+        return new ResponseEntity<User>(updatedUser, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public HttpStatus deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return HttpStatus.OK;
+    }
+}
---
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/service/UserService.java
@@ -0,0 +1,39 @@
+package hu.flowacademy.meetingorganizer.service;
+import hu.flowacademy.meetingorganizer.persistence.model.User;
+import hu.flowacademy.meetingorganizer.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAllUser(){
+        List<User> userList = userRepository.findAll();
+            return new ArrayList<User>();
+    }
+
+    public User findUserById(Long id) throws ResponseStatusException{
+       return userRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) throws ResponseStatusException{
+            userRepository.deleteById(id);
+    }
+
+}
