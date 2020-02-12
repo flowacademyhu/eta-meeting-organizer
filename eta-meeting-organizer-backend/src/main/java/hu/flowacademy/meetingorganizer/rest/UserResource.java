658885c072476d3341e637329bcb63f48d6becd8
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-12 22:28:36 +0100

    renamed

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/UserResource.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/UserResource.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class UserController {
+public class UserResource {
 
     @Autowired
     private UserService userService;
