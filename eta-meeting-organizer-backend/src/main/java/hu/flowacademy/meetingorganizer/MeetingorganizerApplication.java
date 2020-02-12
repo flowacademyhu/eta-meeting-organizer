e2a22d6d33c70ddd00b8d3ecc5a9603d165a2b26
Mihaly Dombi <dombimihaly89@gmail.com>
2020-02-12 11:44:40 +0100

    Just put some basic authentication classes in the project. And a modified apllication yaml. It doesn't work at all. We just put it aside for a while until the auth mechanism gets clearer for us. Maybe we are gonna use another branch and this could be deleted but maybe we are going to continue the work in this branch.

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/MeetingorganizerApplication.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/MeetingorganizerApplication.java
@@ -1,9 +1,13 @@
 package hu.flowacademy.meetingorganizer;
 
+import hu.flowacademy.meetingorganizer.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class MeetingorganizerApplication {
 
 	public static void main(String[] args) {
