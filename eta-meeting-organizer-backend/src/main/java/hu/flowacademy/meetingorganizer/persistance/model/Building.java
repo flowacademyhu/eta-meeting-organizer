65c8ee447e173fc15e99a317ade321bee8da4a3c
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-12 15:14:21 +0100

    @Builder annotation has been added because of initDataLoader

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistance/model/Building.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistance/model/Building.java
@@ -2,6 +2,7 @@ package hu.flowacademy.meetingorganizer.persistance.model;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "buildings")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Building {
