67a6a86b7eadaf78030dc1b41892c677e6fd073b
Peter Horvath <hpeti1988@gmail.com>
2020-02-12 14:34:04 +0100

    Started working on profile page

---
+++ eta-meeting-organizer-frontend/src/app/profile/components/profile.components.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  styles: [`
+  `],
+  template: `
+    <p> ASD </p>
+  `
+})
+
+export class ProfileComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
