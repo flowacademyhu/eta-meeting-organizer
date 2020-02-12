1b10857296351fd9e38bab9109e6b259b321e261
berentepeter <berentepeter@gmail.com>
2020-02-12 09:54:07 +0100

    @Transactional added

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/service/UserService.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/service/UserService.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class UserService {
 
     @Autowired
