82872beaceee407833b9ada32985ac285834f379
Mihaly Dombi <dombimihaly89@gmail.com>
2020-02-12 22:43:45 +0100

    Basic menu view created with sidebar that you can turn on and off. I put the language changer and the logout in the mat-toolbar-row but maybe it ends up in the header later, it will be discussed. I added basic styling too and I put language items in en.json and hu.json that I didn't use though, first the language changing funcionality needs to be put in the main-layout component.

--- eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
@@ -4,38 +4,65 @@ import { Component } from "@angular/core";
   selector: "app-welcome-layout",
   styles: [
     `
-      /* .example-container {
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background: #eee;
-      } */
       mat-icon {
-        color: black;
+        color: #f3f5ed;
+      }
+
+      #navbar-toggle {
+        background-color: #333333;
+      }
+
+      #navbar-toolbar {
+        background-color: #e64b3a;
+      }
+
+      #navbar-list, #nav-sidebar {
+        background-color: #333333;
+        width: 250px;
+      }
+
+      .mat-list-base .mat-list-item {
+        height: 60px;
+        color: #f3f5ed;
+        font-size: 20px;
+      }
+
+      .divider{
+        width:5px;
+        height:auto;
+        display:inline-block;
+      }
+
+      a {
+        font: 400 32px/44px Roboto,"Helvetica Neue",sans-serif;
       }
     `
   ],
   template: `
     <mat-toolbar color="warn">
-      <mat-toolbar-row>
-        <button class="valami"
+      <mat-toolbar-row id="navbar-toolbar">
+        <button id="navbar-toggle"
           type="button"
           aria-label="Toggle sidenav"
-          mat-icon-button
+          mat-raised-button
           (click)="drawer.toggle()"
           color="primary"
         >
         <mat-icon>compare_arrows</mat-icon>
         </button>
+        <div class="ml-auto">
+        <button mat-raised-button>Váltás angol nyelvre</button>
+        <div class="divider"> </div>
+        <button mat-raised-button>Kijelentkezés</button>
+        </div>
       </mat-toolbar-row>
     </mat-toolbar>
     <mat-sidenav-container class="example-container">
-      <mat-sidenav #drawer mode="side" opened role="navigation">
-        <mat-nav-list>
-          <a mat-list-item routerLink="/first">First Component</a>
-          <a mat-list-item routerLink="/second">Second Component</a>
+      <mat-sidenav id="nav-sidebar" #drawer mode="side" opened role="navigation">
+        <mat-nav-list id="navbar-list">
+          <a mat-list-item routerLink="/first">Naptár</a>
+          <a mat-list-item routerLink="/second">Tárgyaló kezelő</a>
+          <a mat-list-item routerLink="/second">Profil</a>
         </mat-nav-list>
       </mat-sidenav>
       <mat-sidenav-content>
@@ -44,4 +71,6 @@ import { Component } from "@angular/core";
     </mat-sidenav-container>
   `
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+
+}
