75d16743cb4511bf71638a693286f8c4460a9ca2
istiFlow <56822076+istiFlow@users.noreply.github.com>
2020-02-13 15:42:13 +0100

    I57 init data loader (#59)
    *  @Builder annotation has been added because of initDataLoader
    
    * @Builder annotation has been added because of initDataLoader
    
    * role and Reservation has been imported
    
    * InitDataLoader filled up with datas to use them to check the backend and after frontend
    
    * moved to persistance package
    
    * moved to persistence package
    
    * moved to persistence package
    
    * moced to persistence package
    
    * moved to persistence package
    
    * moved to persistence package
    
    * role moved to model package
    
    * @Builder added
    
    * renamed
    
    * renamed
    
    * renamed from ..controller to ..resource
    
    * package problem solved
    
    * package problem solved
    
    * made an initdataloader to get datas

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Building.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Building.java
@@ -1,15 +1,15 @@
 package hu.flowacademy.meetingorganizer.persistence.model;
 
-
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "buildings")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Building {
