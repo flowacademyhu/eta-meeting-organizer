cb72caa32f13da4ac0acf84d75ed2af1990aa1ed
claulotta <56822135+claulotta@users.noreply.github.com>
2020-02-13 15:40:26 +0100

    I45 meetingroom entity (#55)
    * Create skeleton for meeting room entity.
    
    * create model and initialize controller, service and repository
    
    * create service
    
    * create resource
    
    * changes in resource in methods to give back response entities
    
    * change response entity to give back meetingroom
    
    * merge origin master
    
    * remove colunm annotation, and other smaller changes asked for
    
    * add required changes
    
    * modify packege name
    
    * required changes
    
    * changes in findOne method in MeetingRoomService
    
    * modification on finOne method

---
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/MeetingRoomResource.java
@@ -0,0 +1,57 @@
+package hu.flowacademy.meetingorganizer.rest;
+
+import hu.flowacademy.meetingorganizer.persistence.model.MeetingRoom;
+import hu.flowacademy.meetingorganizer.service.MeetingRoomService;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/meetingrooms")
+@AllArgsConstructor
+public class MeetingRoomResource {
+
+  private MeetingRoomService meetingRoomService;
+
+  @GetMapping
+  public ResponseEntity<List<MeetingRoom>> findAllMeetingRooms() {
+    List<MeetingRoom> meetingRooms = meetingRoomService.findAll();
+    return new ResponseEntity<>(meetingRooms, HttpStatus.OK);
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<MeetingRoom> findOne(@PathVariable Long id) {
+    Optional<MeetingRoom> meetingRoomOptional = meetingRoomService.findOne(id);
+    return meetingRoomOptional.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(meetingRoomOptional.get());
+  }
+
+  @PostMapping
+  public ResponseEntity<MeetingRoom> createMeetingRoom(@RequestBody MeetingRoom meetingRoom) {
+    meetingRoomService.createMeetingRoom(meetingRoom);
+    return new ResponseEntity<>(meetingRoom, HttpStatus.CREATED);
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<MeetingRoom> updateMeetingRoom(@PathVariable Long id,
+      @RequestBody MeetingRoom meetingRoom) {
+    meetingRoomService.updateMeetingRoom(id, meetingRoom);
+    return ResponseEntity.accepted().build();
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<Void> deleteMeetingRoom(@PathVariable Long id) {
+    meetingRoomService.deleteMeetingRoom(id);
+    return ResponseEntity.noContent().build();
+  }
+}
