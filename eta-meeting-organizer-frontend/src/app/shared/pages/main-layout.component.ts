99c57446f4c1a30baed92289533c2651dde8ae1e
Mihaly Dombi <dombimihaly89@gmail.com>
2020-02-13 16:10:02 +0100

    Global themes are used now

--- eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
@@ -4,26 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-welcome-layout',
   styles: [
     `
-      mat-icon {
-        color: #f3f5ed;
-      }
-
-      #navbar-toggle {
-        background-color: #333333;
-      }
-
-      #navbar-toolbar {
-        background-color: #e64b3a;
-      }
-
       #navbar-list, #nav-sidebar {
-        background-color: #333333;
         width: 250px;
       }
 
       .mat-list-base .mat-list-item {
         height: 60px;
-        color: #f3f5ed;
         font-size: 20px;
       }
 
@@ -33,28 +19,27 @@ import { Component } from '@angular/core';
     `,
   ],
   template: `
-    <mat-toolbar color="warn">
-      <mat-toolbar-row id="navbar-toolbar">
-        <button id="navbar-toggle"
+    <mat-toolbar color="accent">
+      <mat-toolbar-row color="accent">
+        <button color="warn"
           type="button"
           aria-label="Toggle sidenav"
           mat-raised-button
           (click)="drawer.toggle()"
-          color="primary"
         >
         <mat-icon>compare_arrows</mat-icon>
         </button>
       </mat-toolbar-row>
     </mat-toolbar>
-    <mat-sidenav-container>
-      <mat-sidenav id="nav-sidebar" #drawer mode="side" opened role="navigation">
-        <mat-nav-list id="navbar-list">
+    <mat-sidenav-container color="accent">
+      <mat-sidenav id="nav-sidebar" color="accent" #drawer mode="side" opened role="navigation">
+        <mat-nav-list id="navbar-list" color="accent">
           <a mat-list-item routerLink="/first">{{'navbar.calendar' | translate}}</a>
           <a mat-list-item routerLink="/second">{{'navbar.meetingRoomEditor' | translate}}</a>
           <a mat-list-item routerLink="/second">{{'navbar.profile' | translate}}</a>
         </mat-nav-list>
       </mat-sidenav>
-      <mat-sidenav-content>
+      <mat-sidenav-content color="accent">
         <router-outlet></router-outlet>
       </mat-sidenav-content>
     </mat-sidenav-container>
