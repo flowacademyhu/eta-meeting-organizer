4346831c8a534cfd2e6ec4d9ff410afe2d2a3f4a
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-13 12:09:59 +0100

    added material for inputfield and form

--- eta-meeting-organizer-frontend/src/app/shared/material.module.ts
+++ eta-meeting-organizer-frontend/src/app/shared/material.module.ts
@@ -1,19 +1,39 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 @NgModule({
   declarations: [],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
   ],
   imports: [
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
   ],
 })
 export class MaterialModule { }
