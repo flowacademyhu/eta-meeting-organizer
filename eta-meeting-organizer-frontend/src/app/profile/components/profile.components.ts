1de4502e202c84e574186fe8d88f9ede058c63ce
Peter Horvath <hpeti1988@gmail.com>
2020-02-14 13:17:19 +0100

    Managed to get a user's data from backend, in order to display it on a mat-card. Still needs a bit of refactoring css wise.

--- eta-meeting-organizer-frontend/src/app/profile/components/profile.components.ts
+++ eta-meeting-organizer-frontend/src/app/profile/components/profile.components.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '~/app/models/user.model';
+import { ApiCommunicationService } from '~/app/shared/services/api-communication.service';
 
 @Component({
   selector: 'app-profile',
@@ -20,11 +23,12 @@ import {Component, OnInit} from '@angular/core';
   <div class="row justify-content-center align-items-center">
     <div class="col-4">
     <mat-card class="card">
-      <h2>{{'profile.username' | translate}}:</h2>
+      <h2>{{'profile.username' | translate}}: {{(user$ | async)?.name}} </h2>
+      <h2>{{'profile.email' | translate}}: {{(user$ | async)?.email}}</h2>
+      <h2>{{'profile.role' | translate}}: {{(user$ | async)?.role}}</h2>
       <h2>{{'profile.lastName' | translate}}:</h2>
       <h2>{{'profile.firstName' | translate}}:</h2>
-      <h2>{{'profile.email' | translate}}:</h2>
-      <h2>{{'profile.role' | translate}}:</h2>
+
       <button mat-raised-button class="button">{{'profile.delete' | translate}}</button>
     </mat-card>
     </div>
@@ -33,7 +37,14 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class ProfileComponent implements OnInit {
-  constructor() { }
+  public id: number = 1;
+
+  public user$: Observable<User>;
+
+  constructor(private readonly api: ApiCommunicationService) {
+    this.user$ = this.api.user()
+    .getOneUserById(this.id);
+   }
 
   ngOnInit() {
 }
