786637d10443f109087e9443ea485097ba3a68c4
hpeti1988 <56820421+hpeti1988@users.noreply.github.com>
2020-02-14 15:33:15 +0100

    I84 profile backend connection (#89)
    * Managed to get a user's data from backend, in order to display it on a mat-card. Still needs a bit of refactoring css wise.
    
    * Deleted unneccesary css
    
    * Activated routerlink to profile page on navbar.
    
    * Deleted an unneccesary h1 tag
    
    * Deleted default welcome text

--- eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
@@ -37,7 +37,7 @@ import { Component } from '@angular/core';
         <mat-nav-list id="navbar-list" color="accent">
           <a mat-list-item routerLink="/first">{{'navbar.calendar' | translate}}</a>
           <a mat-list-item routerLink="/second">{{'navbar.meetingRoomEditor' | translate}}</a>
-          <a mat-list-item routerLink="/second">{{'navbar.profile' | translate}}</a>
+          <a mat-list-item routerLink="/profile">{{'navbar.profile' | translate}}</a>
         </mat-nav-list>
       </mat-sidenav>
       <mat-sidenav-content color="accent">
