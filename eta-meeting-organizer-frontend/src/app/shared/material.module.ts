e666cc645bcdb49e1d8007f44ca99fbb8cbf027e
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-15 15:17:54 +0100

    mat table added

--- eta-meeting-organizer-frontend/src/app/shared/material.module.ts
+++ eta-meeting-organizer-frontend/src/app/shared/material.module.ts
@@ -11,6 +11,7 @@ import { MatRadioModule} from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
@@ -29,6 +30,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    MatTableModule,
   ],
   imports: [
     MatToolbarModule,
@@ -44,6 +46,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    MatTableModule,
   ],
 })
 export class MaterialModule { }
