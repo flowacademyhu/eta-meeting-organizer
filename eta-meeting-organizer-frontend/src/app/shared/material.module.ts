2f8eea950a724252e8d6ed5daadad8ae48779b10
istiFlow <56822076+istiFlow@users.noreply.github.com>
2020-02-15 23:34:59 +0100

    I93 building editor (#94)
    * set more english word
    
    * set more english word for button and and table
    
    * added a material module to use mat table
    
    * Made a mat table. The table has been filled out by buildings data from backend.
    Every row has a delete button.
    
    * version
    
    * change the selector from app-building to app-building-list
    set async
    delete datasoruce variable

--- eta-meeting-organizer-frontend/src/app/shared/material.module.ts
+++ eta-meeting-organizer-frontend/src/app/shared/material.module.ts
@@ -13,7 +13,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 @NgModule({
   declarations: [],
   exports: [
