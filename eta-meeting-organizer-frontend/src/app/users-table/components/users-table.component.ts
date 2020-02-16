b6bd2c6c64e997c2df4e496a7e54a36dfe0dbd49
Peter Horvath <hpeti1988@gmail.com>
2020-02-16 12:44:52 +0100

    Added table, still needs work to be opeartional.

--- eta-meeting-organizer-frontend/src/app/users-table/components/users-table.component.ts
+++ eta-meeting-organizer-frontend/src/app/users-table/components/users-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiCommunicationService } from '~/app/shared/services/api-communication.service';
 import { User } from './../../models/user.model';
@@ -6,11 +6,32 @@ import { User } from './../../models/user.model';
 @Component({
   selector: 'app-users-table',
   template: `
-  
+  <div class="row justify-content-center">
+      <table mat-table [dataSource]="users$ | async" class="mat-elevation-z8">
+        <ng-container matColumnDef="id">
+          <th mat-header-cell *matHeaderCellDef>{{'profile.id' | translate}} </th>
+          <td mat-cell *matCellDef="let user"> {{user.id}} </td>
+        </ng-container>
+        <ng-container matColumnDef="name">
+          <th mat-header-cell *matHeaderCellDef> {{'profile.username' | translate}} </th>
+          <td mat-cell *matCellDef="let user"> {{user.name}} </td>
+        </ng-container>
+        <ng-container matColumnDef="email">
+          <th mat-header-cell *matHeaderCellDef> {{'profile.email' | translate}} </th>
+          <td mat-cell *matCellDef="let user">{{user.email}}</td>
+        </ng-container>
+        <ng-container matColumnDef="role">
+          <th mat-header-cell *matHeaderCellDef> {{'profile.role' | translate}} </th>
+          <td mat-cell *matCellDef="let user">{{user.role}</td>
+        </ng-container>
+        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+        <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+      </table>
+    </div>
   `
 })
 
-export class UsersTableComponent implements OnInit {
+export class UsersTableComponent {
   public users$: Observable<User[]>;
 
   constructor(private readonly api: ApiCommunicationService) {
