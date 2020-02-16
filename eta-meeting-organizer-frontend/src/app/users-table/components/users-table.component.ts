3a0fc60a6829ea1670a47821eee0f7a8f76b92e2
Peter Horvath <hpeti1988@gmail.com>
2020-02-16 12:32:22 +0100

    Created users variable.

--- eta-meeting-organizer-frontend/src/app/users-table/components/users-table.component.ts
+++ eta-meeting-organizer-frontend/src/app/users-table/components/users-table.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiCommunicationService } from '~/app/shared/services/api-communication.service';
+import { User } from './../../models/user.model';
 
 @Component({
   selector: 'app-users-table',
   template: `
+  
   `
 })
 
 export class UsersTableComponent implements OnInit {
-  constructor() { }
+  public users$: Observable<User[]>;
+
+  constructor(private readonly api: ApiCommunicationService) {
+    this.users$ = this.api.user()
+    .getUsers();
+   }
 
   ngOnInit() { }
 }
