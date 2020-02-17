90ca6ebc5b70026f15f184931a425fc829f59011
berentepeter <berentepeter@gmail.com>
2020-02-17 16:28:56 +0100

    sidebar reamove, new header with buttons

--- eta-meeting-organizer-frontend/src/app/shared/components/header-layout.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/components/header-layout.component.ts
@@ -3,25 +3,24 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
+  styles: [`
+    .email{
+      font-size: 12px;
+    }
+    `,
+  ],
   template: `
-  <mat-toolbar class="header" color="primary">
-    <mat-toolbar-row >
-      <h1>
-        <img src="../../../assets/wysio_arrow.png" height="50" />
-      </h1>
-      <h1 class="white">
-      </h1>
-      <button color="warn" mat-raised-button class="ml-auto"
-              (click)="onLanguageChange()">{{'header.button' | translate}}</button>
-              <div>
-            <a mat-button (click)="logout()" class="white">
-            {{'header.exit' | translate}}
-            </a>
-      </div>
-
-    </mat-toolbar-row>
-  </mat-toolbar>
-`
+  <mat-toolbar class="my-0" color="primary" >
+  <p class="mr-3"><img src="../../../assets/wysio_arrow.png" height="55" /></p>
+  <a class="mr-3" mat-stroked-button routerLink="/first">{{'navbar.calendar' | translate}}</a>
+  <a class="mr-3" mat-stroked-button routerLink="/second">{{'navbar.meetingRoomEditor' | translate}}</a>
+  <a class="mr-3" mat-stroked-button routerLink="/profile">{{'navbar.profile' | translate}}</a>
+  <button mat-button class="ml-auto"(click)="onLanguageChange()">{{'header.button' | translate}}</button>
+  <p class="email">kolbaszjoska@citromail.hu</p>
+  <button mat-stroked-button class="ml-2">
+    <mat-icon>logout</mat-icon>
+  </button>
+</mat-toolbar>`
 })
 
 export class HeaderComponent {
--- eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/pages/main-layout.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome-layout',
   styles: [
-    `
-      #navbar-list, #nav-sidebar {
-        width: 250px;
-      }
-
-      .mat-list-base .mat-list-item {
+    ` .mat-list-base .mat-list-item {
         height: 60px;
         font-size: 20px;
       }
@@ -20,30 +15,7 @@ import { Component } from '@angular/core';
   ],
   template: `
     <app-header></app-header>
-    <mat-toolbar color="accent">
-      <mat-toolbar-row color="accent">
-        <button color="warn"
-          type="button"
-          aria-label="Toggle sidenav"
-          mat-raised-button
-          (click)="drawer.toggle()"
-        >
-        <mat-icon>compare_arrows</mat-icon>
-        </button>
-      </mat-toolbar-row>
-    </mat-toolbar>
-    <mat-sidenav-container color="accent">
-      <mat-sidenav id="nav-sidebar" color="accent" #drawer mode="side" opened role="navigation">
-        <mat-nav-list id="navbar-list" color="accent">
-          <a mat-list-item routerLink="/first">{{'navbar.calendar' | translate}}</a>
-          <a mat-list-item routerLink="/second">{{'navbar.meetingRoomEditor' | translate}}</a>
-          <a mat-list-item routerLink="/profile">{{'navbar.profile' | translate}}</a>
-        </mat-nav-list>
-      </mat-sidenav>
-      <mat-sidenav-content color="accent">
         <router-outlet></router-outlet>
-      </mat-sidenav-content>
-    </mat-sidenav-container>
     <app-footer></app-footer>
   `
 })
