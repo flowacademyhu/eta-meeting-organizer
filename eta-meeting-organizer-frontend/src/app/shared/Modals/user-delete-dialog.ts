b08ad707bd8ae55a4d7143122cf8a7fcbc1c71d8
Peter Horvath <hpeti1988@gmail.com>
2020-02-17 16:46:07 +0100

    Added english and hungarian text for the delete dialog.

--- eta-meeting-organizer-frontend/src/app/shared/Modals/user-delete-dialog.ts
+++ eta-meeting-organizer-frontend/src/app/shared/Modals/user-delete-dialog.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-delete-dialog',
   template: `
-  <h2 mat-dialog-title>Felhasználó törlése</h2>
-  <mat-dialog-content>Biztosan törölni akarja a felhasználót?</mat-dialog-content>
+  <h2 mat-dialog-title>{{'user-delete-dialog.userDelete' | translate}}</h2>
+  <mat-dialog-content>{{'user-delete-dialog.verification' | translate}}</mat-dialog-content>
   <mat-dialog-actions>
-    <button mat-raised-button mat-dialog-close="false">Nem</button>
-    <button mat-raised-button mat-dialog-close="true" color="primary">Igen</button>
+    <button mat-raised-button mat-dialog-close="false">{{'user-delete-dialog.no' | translate}}</button>
+    <button mat-raised-button mat-dialog-close="true" color="primary">{{'user-delete-dialog.yes' | translate}}</button>
   </mat-dialog-actions>
   `
 })
