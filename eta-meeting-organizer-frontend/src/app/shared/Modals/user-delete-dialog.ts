70e588e3f5d4336620b4ad4ef7e5d6d732a873ac
Peter Horvath <hpeti1988@gmail.com>
2020-02-17 11:08:55 +0100

    Added material dialog and created a folder for modals.

---
+++ eta-meeting-organizer-frontend/src/app/shared/Modals/user-delete-dialog.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-delete-dialog',
+  template: ``
+})
+
+export class UserDeleteDialogComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
