f3fbc72f8ab19266b32f03da2e5bc4ca4ac68505
Tamas Vajgely <vajgi90@gmail.com>
2020-02-18 10:32:02 +0100

    Change

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/utils/CookieUtils.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/utils/CookieUtils.java
@@ -23,7 +23,8 @@ public class CookieUtils {
     return Optional.empty();
   }
 
-  public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+  public static void addCookie(HttpServletResponse response, String name, String value,
+      int maxAge) {
     Cookie cookie = new Cookie(name, value);
     cookie.setPath("/");
     cookie.setHttpOnly(true);
@@ -31,7 +32,8 @@ public class CookieUtils {
     response.addCookie(cookie);
   }
 
-  public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+  public static void deleteCookie(HttpServletRequest request, HttpServletResponse response,
+      String name) {
     Cookie[] cookies = request.getCookies();
     if (cookies != null && cookies.length > 0) {
       for (Cookie cookie : cookies) {
