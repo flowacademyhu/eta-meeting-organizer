1ea72d9188d99e39bec9742ccf9017510d5b074c
Mihaly Dombi <dombimihaly89@gmail.com>
2020-02-18 10:04:49 +0100

    I pulled out CrossOrigin annotation from Reservation resource as requested.

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/ReservationResource.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/ReservationResource.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/reservations")
 @AllArgsConstructor
-@CrossOrigin("*")
 public class ReservationResource {
 
   private ReservationService reservationService;
