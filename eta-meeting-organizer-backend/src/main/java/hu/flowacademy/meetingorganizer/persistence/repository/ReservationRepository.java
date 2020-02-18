281df1ca8cb112da158f64e3af7559f874c1fe02
dombimihaly89 <56822065+dombimihaly89@users.noreply.github.com>
2020-02-18 11:32:48 +0100

    I111-Added methods and endpoint to find reservations by userId. (#118)
    * Added methods and endpoint to find reservations by userId.
    
    * Added changes as per requested in the pull request comment section.

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/ReservationRepository.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/ReservationRepository.java
@@ -1,10 +1,14 @@
 package hu.flowacademy.meetingorganizer.persistence.repository;
 
 import hu.flowacademy.meetingorganizer.persistence.model.Reservation;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+  List<Reservation> findByUser_Id(Long id);
 
 }
