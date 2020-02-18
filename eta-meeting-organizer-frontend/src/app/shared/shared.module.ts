ed22cce2fccfcfb1c1686e48f4a3c462c62c8c4d
istiFlow <56822076+istiFlow@users.noreply.github.com>
2020-02-18 11:32:15 +0100

    I96 meeting room registration (#106)
    * Added a button what we can use to open a modal to register a room
    
    * import MatDialogModule
    
    * set a new navigation to open meetingrooms
    
    * set MeetingRoomRegister to declarations
    
    * set more words
    
    * set more words
    
    * created a modal folder for meeting room registration
    
    * fixed the placeholder problem.
    
    * delete empty lines, fixed language problem

--- eta-meeting-organizer-frontend/src/app/shared/shared.module.ts
+++ eta-meeting-organizer-frontend/src/app/shared/shared.module.ts
@@ -10,12 +10,14 @@ import { ApiCommunicationService } from '~/app/shared/services/api-communication
 import { ConfigurationService } from '~/app/shared/services/configuration.service';
 import { HeaderComponent } from './components/header-layout.component';
 import { BuildingRegisterComponent } from './Modals/building-register.component';
+import { MeetingRoomRegisterComponent } from './Modals/meeting-room-register.component';
 
 @NgModule({
   declarations: [
     MainLayoutComponent,
     HeaderComponent,
     FooterComponent,
+    MeetingRoomRegisterComponent,
     BuildingRegisterComponent,
   ],
   exports: [
