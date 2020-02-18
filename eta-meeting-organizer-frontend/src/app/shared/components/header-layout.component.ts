ef8aa895a562b5bac56a33d672270b4fb12a737d
Berente Péter <40863181+berentepeter@users.noreply.github.com>
2020-02-18 12:33:08 +0100

    I 105 new header (#109)
    * header and footer with logout and language select buttons
    
    * sidebar reamove,
    new header with buttons
    
    * new logout icon added
    
    * compíny logo repositioned
    header stickyness added

--- eta-meeting-organizer-frontend/src/app/shared/components/header-layout.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/components/header-layout.component.ts
@@ -3,25 +3,40 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-  <mat-toolbar class="header" color="primary">
-    <mat-toolbar-row >
-      <h1>
-        <img src="../../../assets/wysio_arrow.png" height="50" />
-      </h1>
-      <h1 class="white">
-      </h1>
-      <button color="warn" mat-raised-button class="ml-auto"
-              (click)="onLanguageChange()">{{'header.button' | translate}}</button>
-              <div>
-            <a mat-button (click)="logout()" class="white">
-            {{'header.exit' | translate}}
-            </a>
-      </div>
+  styles: [`
+    #row {
+  height: 60px;
+  position: fixed;
+  top: 0px;
+  text-align: center;
+  position: fixed;
+  z-index:999;
+  font-size: smaller;
+  align-items: center;
+  position: sticky;
+  }
+  .email{
+      font-size: 12px;
+    }
+  #logout{
 
-    </mat-toolbar-row>
-  </mat-toolbar>
-`
+  padding-top: 15px;
+
+    }
+    `,
+  ],
+  template: `
+  <mat-toolbar id="row" class="my-0" color="accent" >
+  <p class="mr-3"><img src="../../../assets/wysio_arrow.png" height="55" /></p>
+  <a class="mr-3" mat-stroked-button routerLink="/first">{{'navbar.calendar' | translate}}</a>
+  <a class="mr-3" mat-stroked-button routerLink="/second">{{'navbar.meetingRoomEditor' | translate}}</a>
+  <a class="mr-3" mat-stroked-button routerLink="/profile">{{'navbar.profile' | translate}}</a>
+  <button mat-button class="ml-auto"(click)="onLanguageChange()">{{'header.button' | translate}}</button>
+  <p class="email">kolbaszjoska@citromail.hu</p>
+  <button mat-button id="logout" class="ml-2">
+  <p><img padding="20" src="../../../assets/logout.png" height="50"/></p>
+  </button>
+</mat-toolbar>`
 })
 
 export class HeaderComponent {
