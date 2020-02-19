0a80160586f1847f3dfbcdc08c0900666e313e89
hpeti1988 <56820421+hpeti1988@users.noreply.github.com>
2020-02-19 17:14:59 +0100

    I91 users table (#119)
    * Created basic setup for users table.
    
    * Added routing for users table in app-routing.
    
    * Created users variable.
    
    * Added displayedColumns for column names
    
    * Added id to the language json.
    
    * Added table, still needs work to be opeartional.
    
    * Had to add cross origin annotation in order to get data on the frontend.
    
    * Basic form of users table seems to be working.
    
    * Had to add utf8 encoding to db
    
    * Delete method works, however the page won't refresh the table.
    
    * Deleted unnecessary lines
    
    * Added material dialog and created a folder for modals.
    
    * Created a modal for user deletion. The delete method itself works, however I need to achieve page refreshing.
    
    * Added BehaviourSubject to UserApiConnector, which is then utilised during deletion of a users in order to refresh the current page after a delete method.
    
    * Added a trash icon instead of delete text
    
    * Changed last columns identifier to action, and also added a user icon for the to be implemented validation method
    
    * Fixed a few lint issues
    
    * Added english and hungarian text for the delete dialog.
    
    * Centered column texts
    
    * Fixed a missing space
    
    * Fixed a missing new line in users-table-component
    
    * Restored userApiConnector to its orginal state
    
    * Created a user service for the behavioursubject and the different http  requests
    
    * Added UserService to sharedmodule's providers
    
    * Refactored users-table component, changed path in app routing.
    
    * Changed buttons to mat icon buttons
    
    * Had to do npm i because of auth
    
    * Deleted an unneccessary css class
    
    * Did an npm i
    
    * Had to change id type to corresponde with the new id coming from the token. Also connected users-table to corresponding button.
    
    * Added myemail address to admin field on the backend side

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/config/SecurityConfig.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+        .csrf().disable()
         .cors().disable()
         .formLogin().disable()
         .httpBasic().disable()
