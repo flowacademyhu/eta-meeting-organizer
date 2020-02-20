1f94d07409d5e83f6ca78dba6b06b9cec9cd7039
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-20 15:08:37 +0100

    made 2 more connector

--- eta-meeting-organizer-frontend/src/app/shared/api-connectors/BuildingApiConnector.ts
+++ eta-meeting-organizer-frontend/src/app/shared/api-connectors/BuildingApiConnector.ts
@@ -1,27 +1,27 @@
 import { Observable } from 'rxjs';
 import { Building } from '~/app/models/building.model';
 import { AbstractApiConnector } from '~/app/shared/api-connectors/AbstractApiConnector';
-
 export class BuildingApiConnector extends AbstractApiConnector {
   protected readonly apiRoute: string = `${this.apiBaseUrl}`;
-
   public getBuildings(): Observable<Building[]> {
-    return this.http.get<Building[]>(`${this.apiRoute}/buildings`);
+    return this.http.get<Building[]>(`${this.apiRoute}/buildings/`);
   }
-
   public getOneBuilding(id: number): Observable<Building> {
     return this.http.get<Building>(`${this.apiRoute}/buildings/` + id);
   }
-
   public postBuilding(building: Building): Observable<Building> {
-    return this.http.post<Building>(`${this.apiRoute}/buildings`, building);
+    return this.http.post<Building>(`${this.apiRoute}/buildings/`, + building);
   }
-
   public updateBuilding(id: number, building: Building) {
     return this.http.put(`${this.apiRoute}/buildings/` + id, building);
   }
-
-  public deleteMeetingRoomById(id: number) {
+  public deleteBuildingId(id: number) {
     return this.http.delete(`${this.apiRoute}/buildings/` + id);
+  }
+  public findAllCities() {
+    return this.http.get<Building[]>(`${this.apiRoute}/buildings/cities/names`);
+  }
+  public findByCity(city: string): Observable<Building[]> {
+    return this.http.get<Building[]>(`${this.apiRoute}/buildings/cities?city=${city}`);
   }
 }
