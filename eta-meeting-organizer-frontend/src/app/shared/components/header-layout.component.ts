6ddff38c5fae2113130dfb81c50dcbb1e2449719
Tamas Vajgely <vajgi90@gmail.com>
2020-02-20 17:29:33 +0100

    Travis correction

--- eta-meeting-organizer-frontend/src/app/shared/components/header-layout.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/components/header-layout.component.ts
@@ -33,12 +33,15 @@ import { AuthService } from '../services/auth.service';
 ],
   template: `
   <mat-toolbar id="row" class="my-0" color="accent" >
-  <a class="mr-3"  routerLink="/calendar"><img src="../../../assets/wysio_arrow.png" height="55" /></a>
-  <a class="mr-3" *ngIf="isToken" mat-stroked-button routerLink="/calendar">{{'navbar.calendar' | translate}}</a>
+  <a class="mr-3"
+  routerLink="/calendar"><img src="../../../assets/wysio_arrow.png" height="55" /></a>
+  <a class="mr-3"
+  *ngIf="isToken" mat-stroked-button routerLink="/calendar">{{'navbar.calendar' | translate}}</a>
   <a class="mr-3"
   *ngIf="isAdmin"
    mat-stroked-button routerLink="/meetingroom">{{'navbar.meetingRoomEditor' | translate}}</a>
-  <a class="mr-3" *ngIf="isAdmin" mat-stroked-button routerLink="/profile">{{'navbar.profile' | translate}}</a>
+  <a class="mr-3"
+  *ngIf="isAdmin" mat-stroked-button routerLink="/profile">{{'navbar.profile' | translate}}</a>
   <a class="mr-3"
   *ngIf="isAdmin" mat-stroked-button routerLink="/building-register">{{'navbar.buildingEditor' | translate}}</a>
   <button mat-button class="ml-auto"(click)="onLanguageChange()">{{'header.button' | translate}}</button>
