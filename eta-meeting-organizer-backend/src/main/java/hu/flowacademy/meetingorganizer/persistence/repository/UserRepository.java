78b54d0338bd993f5cafac25025c3c09cd6c8694
hpeti1988 <56820421+hpeti1988@users.noreply.github.com>
2020-02-20 17:08:18 +0100

    I135 user verification (#139)
    * Added update method into user service
    
    * Added verification dialog and snackbars to delete and verificaiton dialog.
    
    * Added missing import
    
    * Added verification dialog and an update to the existing users-table
    
    * Added UserVerificationDialogComponent to shared modules declaration and also fixed some lint problems
    
    * Added a missing new line
    
    * Added mat snackbar
    
    * Fixed a line for verification
    
    * Added translation lines
    
    * Made a little change to the user update in order to be able to verify on first sign in
    
    * Added language texts and changed a button text in the header to show the correct page name
    
    * Updated snackBars to show the text according to the selected language
    
    * Fixed a lint problem
    
    * Deleted unneccessary parameter from user update and a not used method from user repo
    
    * Changed .get() to .orelseThrow() in user update

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/UserRepository.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/UserRepository.java
@@ -10,5 +10,4 @@ public interface UserRepository extends JpaRepository<User, String> {
 
   Optional<User> findByUsername(String username);
 
-  Optional<User> findById(String id);
 }
--- eta-meeting-organizer-frontend/src/app/shared/material.module.ts
+++ eta-meeting-organizer-frontend/src/app/shared/material.module.ts
@@ -12,6 +12,7 @@ import { MatRadioModule} from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
@@ -32,6 +33,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSlideToggleModule,
     MatTableModule,
     MatToolbarModule,
+    MatSnackBarModule,
   ],
   imports: [
     FormsModule,
@@ -49,6 +51,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSlideToggleModule,
     MatTableModule,
     MatToolbarModule,
+    MatSnackBarModule,
   ],
 })
 export class MaterialModule { }
