e58fd68b791b5edcbbe8c9584a3beb162f8954bb
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-20 09:50:09 +0100

    travis fixed

--- eta-meeting-organizer-frontend/src/app/meeting-room/components/meeting-room.component.ts
+++ eta-meeting-organizer-frontend/src/app/meeting-room/components/meeting-room.component.ts
@@ -6,7 +6,6 @@ import { MeetingRoomDeleteComponent } from './../../shared/Modals/meeting-room-d
 import { MeetingRoomRegisterComponent } from './../../shared/Modals/meeting-room-register.component';
 import { MeetingRoomService } from './../../shared/services/meeting-room.service';
 
-
 @Component({
   selector: 'app-meeting-room-listing',
   styles: [`
