fe4f766396af45d0ab048aa5b84f5509426d3d34
berentepeter <berentepeter@gmail.com>
2020-02-20 10:20:43 +0100

    new design created (not a final version)

--- eta-meeting-organizer-frontend/src/app/login/components/login.component.ts
+++ eta-meeting-organizer-frontend/src/app/login/components/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '~/environment/environment';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
         min-height: calc(100vh - 60px);
       }
       #customBtn {
+       
         display: inline-block;
         background: rgb(59,59,59);
         color: rgb(243, 245, 237);
@@ -21,14 +23,15 @@ import { Component } from '@angular/core';
         background: transparent 5px 50% no-repeat;
         display: inline-block;
         vertical-align: middle;
-        width: 35px;
+        width: 30px;
         height: 35px;
+        padding-bottom: 5px;
       }
       span.buttonText {
         color: white;
         display: inline-block;
         vertical-align: middle;
-        padding-left: 42px;
+        padding-left: 15px;
         padding-right: 42px;
         font-size: 14px;
         font-weight: bold;
@@ -63,14 +66,16 @@ import { Component } from '@angular/core';
             <img id="image" src="../../../assets/wysio_arrow.png"/>
 
           <mat-card-content>
-            <button mat-stroked-button id="customBtn" class="customGPlusSignIn" style="cursor: pointer;">
-              <span class="icon" id="googleicon">
+          <a href="${environment.googleAuthLink}">
+            <button mat-stroked-button id="customBtn" class="customGPlusSignIn">
+              <span class="icon">
                 <img src="../../../assets/googlelogo.png" />
               </span>
-              <span (click)="signInWithGoogle()">{{
+              <span class="buttonText">{{
                 "login.loginButton" | translate
               }}</span>
             </button>
+          </a>
           </mat-card-content>
         </mat-card>
       </div>
