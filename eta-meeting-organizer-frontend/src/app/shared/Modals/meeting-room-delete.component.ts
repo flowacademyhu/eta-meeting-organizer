c7db200e2908782956ade9b120e78f56f1c60e77
Istvan Dumitras <d.steven@protonmail.ch>
2020-02-24 15:12:54 +0100

    modified the delete modals, and created a snack bar for it.

--- eta-meeting-organizer-frontend/src/app/shared/Modals/meeting-room-delete.component.ts
+++ eta-meeting-organizer-frontend/src/app/shared/Modals/meeting-room-delete.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-meeting-room-delete',
   template: `
- <h2 mat-dialog-title>{{'meeting-room-delete.meetingRoomDelete' | translate}}</h2>
+  <h3 md-dialog-title>{{'meeting-room-delete.meetingRoomDelete' | translate}}</h3>
   <mat-dialog-content>{{'meeting-room-delete.verification' | translate}}</mat-dialog-content>
-  <mat-dialog-actions>
-    <button mat-raised-button mat-dialog-close="false">{{'meeting-room-delete.no' | translate}}</button>
-    <button mat-raised-button mat-dialog-close="true" color="primary">{{'meeting-room-delete.yes' | translate}}</button>
+  <mat-dialog-actions >
+    <button mat-stroked-button mat-dialog-close="false">Cancel</button>
+    <button mat-stroked-button mat-dialog-close="true" color="primary" (click)="openSnackBar()">Delete</button>
   </mat-dialog-actions>
+
 `
 })
 
 export class MeetingRoomDeleteComponent {
+  constructor(
+    private readonly snackBar: MatSnackBar,
+    private readonly translate: TranslateService) {}
 
+  public openSnackBar() {
+    this.snackBar.open(this.translate.instant(`meeting-room-delete.deleteSnackBar`), '', {
+      duration: 2500
+    });
+  }
 }
