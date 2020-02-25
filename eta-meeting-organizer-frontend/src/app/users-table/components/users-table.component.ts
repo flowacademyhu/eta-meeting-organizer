c24dc18d976c9deb3b3abcbc7d571dab0ab6423d
Peter Horvath <hpeti1988@gmail.com>
2020-02-25 11:31:57 +0100

    Fixed a travis train bug.

--- eta-meeting-organizer-frontend/src/app/users-table/components/users-table.component.ts
+++ eta-meeting-organizer-frontend/src/app/users-table/components/users-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable, Subscription } from 'rxjs';
 import { UserVerificationDialogComponent } from '~/app/shared/Modals/user-verification-dialog';
