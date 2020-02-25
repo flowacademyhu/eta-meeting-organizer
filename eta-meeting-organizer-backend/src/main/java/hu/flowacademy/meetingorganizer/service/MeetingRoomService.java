a6f44835f134e028238859ff42f07e7c2dadbfe6
istiFlow <56822076+istiFlow@users.noreply.github.com>
2020-02-25 07:17:12 +0100

    I166 dto fix (#168)
    * created a buildingname
    
    * cheanged with Robi
    
    * changed with Robi
    
    * changed the dto's to work with frontend
    
    * after Tomi's pull request I fixed my code

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/service/MeetingRoomService.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/service/MeetingRoomService.java
@@ -5,6 +5,7 @@ import hu.flowacademy.meetingorganizer.persistence.model.dto.MeetingRoomDTO;
 import hu.flowacademy.meetingorganizer.persistence.repository.MeetingRoomRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -17,33 +18,31 @@ public class MeetingRoomService {
 
   private MeetingRoomRepository meetingRoomRepository;
 
-  public List<MeetingRoom> findAll(Integer pageNumber, Integer pageSize) {
+  public List<MeetingRoomDTO> findAll(Integer pageNumber, Integer pageSize) {
     return meetingRoomRepository
-        .findAll(PageRequest.of(pageNumber, pageSize)).getContent();
+        .findAll(PageRequest.of(pageNumber, pageSize)).getContent().stream().map(MeetingRoomDTO::new).collect(
+            Collectors.toList());
   }
 
-  public Optional<MeetingRoom> findOne(Long id) {
-    return meetingRoomRepository.findById(id);
-  }
-
-  public MeetingRoom createMeetingRoom(MeetingRoom meetingRoom) {
-    return meetingRoomRepository.save(meetingRoom);
+  public Optional<MeetingRoomDTO> findOne(Long id) {
+    return meetingRoomRepository.findById(id).map(MeetingRoomDTO::new);
   }
 
   public void deleteMeetingRoom(Long id) {
     meetingRoomRepository.deleteById(id);
   }
 
-  public MeetingRoom updateMeetingRoom(Long id, MeetingRoom meetingRoom) {
+  public MeetingRoomDTO updateMeetingRoom(Long id, MeetingRoomDTO dto) {
+    MeetingRoom meetingRoom = dto.toEntity();
     meetingRoom.setId(id);
-    return meetingRoomRepository.save(meetingRoom);
-  }
-
-  public List<MeetingRoom> findByBuildingId(Long id) {
-    return meetingRoomRepository.findByBuilding_Id(id);
-  }
-
-  public MeetingRoomDTO create(MeetingRoom meetingRoom) {
     return new MeetingRoomDTO(meetingRoomRepository.save(meetingRoom));
+  }
+
+  public List<MeetingRoomDTO> findByBuildingId(Long id) {
+    return meetingRoomRepository.findByBuilding_Id(id).stream().map(MeetingRoomDTO::new).collect(Collectors.toList());
+  }
+
+  public MeetingRoomDTO create(MeetingRoomDTO meetingRoom) {
+    return new MeetingRoomDTO(meetingRoomRepository.save(meetingRoom.toEntity()));
   }
 }
