0e6605b5988615aa20839be0a865bdffd04047b3
Berente Péter <40863181+berentepeter@users.noreply.github.com>
2020-02-26 20:55:38 +0100

    I 155 pagination (#176)
    * sorting, filtering, pagination
    
    * pagination, filtering, sorting on pages:
    -users
    -meeting room
    -building
    
    * removed blank lined
    
    * removed an unnecessary module
    
    * conflict solved
    
    * building component updated
    
    * missing comma
    
    * typo fixed
    
    * another, totally similar typo
    
    * deleted unnecessary lines
    
    * conflicts resolved
    
    * removed backend pagination
    
    * new funcions(findAllByOrderById) added
    
    * added blank lines above functions in repository interfaces
    
    * a sort header added (to buildingname)
    
    * bugfix
    
    * user table text format
    
    * text size changed

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/UserResource.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/rest/UserResource.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,10 +25,8 @@ public class UserResource {
   private UserService userService;
 
   @GetMapping
-  public ResponseEntity<List<User>> findAll(
-      @RequestParam(defaultValue = "0") Integer pageNumber,
-      @RequestParam(defaultValue = "10") Integer pageSize) {
-    return new ResponseEntity<>(userService.findAll(pageNumber, pageSize), HttpStatus.OK);
+  public ResponseEntity<List<User>> findAll() {
+    return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
   }
 
   @GetMapping("/{id}")
--- eta-meeting-organizer-frontend/src/app/building/components/building.component.ts
+++ eta-meeting-organizer-frontend/src/app/building/components/building.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable, Subscription } from 'rxjs';
 import { Building } from '~/app/models/building.model';
 import { BuildingDeleteDialogComponent } from '~/app/shared/Modals/building-delete-dialog';
 import { BuildingRegisterComponent } from '~/app/shared/Modals/building-register.component';
 import { BuildingUpdateDialogComponent } from '~/app/shared/Modals/building-update-dialog';
+import { ApiCommunicationService } from '~/app/shared/services/api-communication.service';
 import { BuildingService } from './../../shared/services/building.service';
 
 @Component({
   selector: 'app-building-list',
   styles: [`
+    table {
+      width: 100%;
+      overflow-x: auto;
+      overflow-y: hidden;
+    }
+    th.mat-header-cell {
+      text-align: left;
+      max-width: 300px!important;
+    }
+    .row {
+      min-height: calc(100vh - 60px);
+    }
     .center {
       text-align: center;
       font-size: larger;
     }
-
-    table {
-      width: 100%;
-    }
-
     .addButton {
       width: 100%;
     }
@@ -28,22 +39,25 @@ import { BuildingService } from './../../shared/services/building.service';
           (click)="postDialog()">
           <mat-icon>add</mat-icon>
     </button>
-    <div class="row justify-content-center">
-      <table mat-table [dataSource]="building$ | async" class="mat-elevation-z8">
+    <div>
+    <mat-form-field>
+    <input matInput type="text" (keyup)="doFilter($event.target.value)" placeholder="Filter">
+  </mat-form-field>
+      <table mat-table [dataSource]="dataSource" class="mat-elevation-z8" matSort>
       <ng-container matColumnDef="buildingName">
           <th mat-header-cell *matHeaderCellDef class="center">{{'building.buildingName' | translate}}</th>
           <td mat-cell *matCellDef="let building"> {{building.buildingName}} </td>
         </ng-container>
         <ng-container matColumnDef="city">
-          <th mat-header-cell *matHeaderCellDef class="center">{{'building.city' | translate}}</th>
+          <th mat-header-cell *matHeaderCellDef class="center" mat-sort-header>{{'building.city' | translate}} </th>
           <td mat-cell *matCellDef="let building"> {{building.city}} </td>
         </ng-container>
         <ng-container matColumnDef="address">
-          <th mat-header-cell *matHeaderCellDef class="center"> {{'building.address' | translate}} </th>
+          <th mat-header-cell *matHeaderCellDef class="center" mat-sort-header> {{'building.address' | translate}} </th>
           <td mat-cell *matCellDef="let building"> {{building.address}} </td>
         </ng-container>
-        <ng-container matColumnDef="action">
-          <th mat-header-cell *matHeaderCellDef></th>
+        <ng-container matColumnDef="delete">
+          <th mat-header-cell *matHeaderCellDef class="center" mat-sort-header> {{'building.edit' | translate}} </th>
           <td mat-cell *matCellDef="let building">
            <button mat-icon-button color="accent" (click)="updateDialog(building)">
           <mat-icon aria-label="Edit">
@@ -58,26 +72,56 @@ import { BuildingService } from './../../shared/services/building.service';
           </td>
         </ng-container>
         <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-        <tr mat-row *matRowDef="let row; columns: displayedColumns;" align="center"></tr>
+        <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
       </table>
+      <mat-paginator
+        [pageSize]="5"
+        [pageSizeOptions]="[5, 10, 20]"
+        showFirstLastButton>
+      </mat-paginator>
     </div>
   `
 })
 
-export class BuildingComponent implements OnInit, OnDestroy {
+export class BuildingComponent implements OnInit, OnDestroy, AfterViewInit {
+
   public building$: Observable<Building[]>;
-  public displayedColumns: string[] = ['buildingName', 'city', 'address', 'action'];
+  public displayedColumns: string[] = ['buildingName', 'city', 'address', 'delete'];
   public deleteUnsub: Subscription;
   public updateUnsub: Subscription;
   public postUnsub: Subscription;
-  constructor(private readonly buildingService: BuildingService,
-              private readonly dialog: MatDialog) { }
 
-   public ngOnInit() {
-     this.buildingService.getAllBuildings();
-     this.building$ = this.buildingService
-     .buildingSub;
-   }
+  @ViewChild(MatSort) public sort: MatSort;
+  @ViewChild(MatPaginator) public paginator: MatPaginator;
+
+  constructor(private readonly api: ApiCommunicationService,
+              private readonly dialog: MatDialog,
+              private readonly buildingService: BuildingService) {
+                this.building$ = this.api.building()
+                                         .getBuildings();
+  }
+
+  public dataSource: MatTableDataSource<Building> = new MatTableDataSource<Building>();
+
+  public dataSub: Subscription;
+
+  public ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSub = this.buildingService.getBuildings()
+      .subscribe((res) => {
+        this.dataSource.data = (res as unknown as Building[]);
+      });
+    }
+
+  public ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+ }
+
+ public doFilter = (value: string) => {
+   this.dataSource.filter = value.trim()
+    .toLocaleLowerCase();
+ }
 
   public postDialog(): void {
     const dialogRef = this.dialog.open(BuildingRegisterComponent, {
@@ -115,6 +159,7 @@ export class BuildingComponent implements OnInit, OnDestroy {
    }
 
    public ngOnDestroy(): void {
+    this.dataSub.unsubscribe();
     if (this.deleteUnsub) {
       this.deleteUnsub.unsubscribe();
     }
