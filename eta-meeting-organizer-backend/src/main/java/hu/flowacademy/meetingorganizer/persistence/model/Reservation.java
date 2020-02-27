87463cacca13a6e67526d6fd5de2ca3b3920659b
claulotta <claulotta@gmail.com>
2020-02-27 14:44:51 +0100

    repair bad conflict resolution

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Reservation.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Reservation.java
@@ -1,24 +1,17 @@
 package hu.flowacademy.meetingorganizer.persistence.model;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "reservations")
@@ -32,17 +25,11 @@ public class Reservation {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
   @NotNull
-  private LocalDateTime startingTime;
+  private Long startingTime;
 
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
   @NotNull
-  private LocalDateTime endingTime;
+  private Long endingTime;
 
   @NotNull
   private String title;
