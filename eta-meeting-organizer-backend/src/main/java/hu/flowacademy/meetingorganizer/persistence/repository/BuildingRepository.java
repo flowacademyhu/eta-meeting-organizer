19f3cbdde2ebfc736a25aec1f1fb06bcc4d38199
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-02 17:30:43 +0100

    added a deleteByIdIN to delete a bulk of id's (checkboxes)

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/BuildingRepository.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/BuildingRepository.java
@@ -3,6 +3,7 @@ package hu.flowacademy.meetingorganizer.persistence.repository;
 import hu.flowacademy.meetingorganizer.persistence.model.Building;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
