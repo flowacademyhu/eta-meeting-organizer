3ffc3b407812349b2214534bb63f2d6250633192
Istvan Dumitras <d.steven@protonmail.ch>
2020-03-02 17:32:12 +0100

    created a deleteByIdIn

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/MeetingRoomRepository.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/MeetingRoomRepository.java
@@ -3,6 +3,7 @@ package hu.flowacademy.meetingorganizer.persistence.repository;
 import hu.flowacademy.meetingorganizer.persistence.model.MeetingRoom;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +12,7 @@ public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Long> 
   List<MeetingRoom> findByBuilding_Id(Long id);
 
   List<MeetingRoom> findAllByOrderById();
+
+  @Modifying
+  void deleteByIdIn(List<Long> id);
 }
