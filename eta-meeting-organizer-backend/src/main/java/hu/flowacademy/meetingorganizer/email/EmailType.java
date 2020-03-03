63d6a2fcd2939cd40ce7337598a56dbf69e5b469
Tamas Vajgely <vajgi90@gmail.com>
2020-03-03 18:05:35 +0100

    First commit of email expansion

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/email/EmailType.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/email/EmailType.java
@@ -1,6 +1,18 @@
 package hu.flowacademy.meetingorganizer.email;
 
 public enum EmailType {
+  CREATE("new-meeting.html"),
+  UPDATE("update-meeting.html"),
+  DELETE("delete-meeting.html"),
+  VERIFY("verify-registration.html");
 
-  TEXT, HTML
+  private String templateName;
+
+  EmailType(String templateName) {
+    this.templateName = templateName;
+  }
+
+  public String getTemplateName() {
+    return templateName;
+  }
 }
--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/email/GmailService.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/email/GmailService.java
@@ -1,28 +1,43 @@
 package hu.flowacademy.meetingorganizer.email;
 
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import java.util.Locale;
+import java.util.Map;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Slf4j
-@RequiredArgsConstructor
-public class GmailService implements EmailService {
+@Service
+@AllArgsConstructor
+public class GmailService {
 
-  @NonNull
+
   private final JavaMailSender mailSender;
-  @NonNull
-  private final EmailFactory emailFactory;
+  private final TemplateEngine templateEngine;
 
   @Async
-  @Override
-  public void send(String to, String subject, EmailType emailType, Object... bodyParams) {
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setTo(to);
-    message.setSubject(subject);
-    message.setText(emailFactory.buildContent(emailType, bodyParams).getValue());
-    mailSender.send(message);
+  public void send(String to, String subject, String templateName, Map<String, Object> params) {
+    send(to, subject, templateEngine.process(templateName, new Context(Locale.ENGLISH, params)));
+  }
+
+  private void send(String to, String subject, String text) {
+    try {
+      final MimeMessage mimeMessage = this.mailSender.createMimeMessage();
+      final MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+      message.setTo(to);
+      message.setSubject(subject);
+      message.setText(text, true);
+      mailSender.send(mimeMessage);
+    } catch (MessagingException e) {
+      e.printStackTrace();
+    }
+
   }
 }
