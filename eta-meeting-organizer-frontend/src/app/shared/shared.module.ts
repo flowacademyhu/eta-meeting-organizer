9ad80caf2e98fec3d332e9019360bbab95fbb211
istiFlow <56822076+istiFlow@users.noreply.github.com>
2020-03-03 14:42:40 +0100

    I222 modal for checkbox (#234)
    * created a modal for building components
    
    * created a dialog for checkbox
    
    * created a dialog for checkbox
    
    * added for share modul
    
    * travis fixed
    
    * set warning message in modal
    
    * set the delete button red only if a checkbox checked
    
    * delete unused method
    
    * if a checkbo unchecked then the delete button will be disable
    
    * set the trash and checkbox to center
    
    * console log deleted
    
    * fixed a bug, when I checked a checkbox to delete a buliding but i pressed the cancel then the
    checkbox didn't set back to unchecked..yet!

--- eta-meeting-organizer-frontend/src/app/shared/shared.module.ts
+++ eta-meeting-organizer-frontend/src/app/shared/shared.module.ts
@@ -13,6 +13,7 @@ import { MainLayoutComponent } from '~/app/shared/pages/main-layout.component';
 import { ApiCommunicationService } from '~/app/shared/services/api-communication.service';
 import { ConfigurationService } from '~/app/shared/services/configuration.service';
 import { HeaderComponent } from './components/header-layout.component';
+import { BuildingCheckboxComponent } from './Modals/building-checkbox-delete.component';
 import { BuildingDeleteDialogComponent } from './Modals/building-delete-dialog';
 import { BuildingRegisterComponent } from './Modals/building-register.component';
 import { BuildingUpdateDialogComponent } from './Modals/building-update-dialog';
@@ -51,6 +52,7 @@ import { UserService } from './services/user.service';
     ReservationInfoComponent,
     ReservationDeleteComponent,
     ReservationTimeUpdateComponent,
+    BuildingCheckboxComponent,
   ],
   exports: [
     CommonModule,
