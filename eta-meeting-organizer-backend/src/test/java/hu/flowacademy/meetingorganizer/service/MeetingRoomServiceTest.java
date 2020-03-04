86584b4e11097685c761f642fb1de9f0c42d0c79
claulotta <claulotta@gmail.com>
2020-03-04 23:10:30 +0100

    modify findOne, findAll and creta tests

--- eta-meeting-organizer-backend/src/test/java/hu/flowacademy/meetingorganizer/service/MeetingRoomServiceTest.java
+++ eta-meeting-organizer-backend/src/test/java/hu/flowacademy/meetingorganizer/service/MeetingRoomServiceTest.java
@@ -10,7 +10,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import hu.flowacademy.meetingorganizer.exception.MeetingRoomNotFoundException;
+import hu.flowacademy.meetingorganizer.persistence.model.Building;
 import hu.flowacademy.meetingorganizer.persistence.model.MeetingRoom;
+import hu.flowacademy.meetingorganizer.persistence.model.dto.BuildingDTO;
 import hu.flowacademy.meetingorganizer.persistence.model.dto.MeetingRoomDTO;
 import hu.flowacademy.meetingorganizer.persistence.repository.MeetingRoomRepository;
 import java.util.Arrays;
@@ -35,6 +37,7 @@ public class MeetingRoomServiceTest {
   private static MeetingRoom meetingRoom;
   private static MeetingRoomDTO meetingRoomDTO1;
   private static MeetingRoomDTO meetingRoomDTO2;
+  private static BuildingDTO building1;
 
   @Mock
   private MeetingRoomRepository meetingRoomRepository;
@@ -49,55 +52,55 @@ public class MeetingRoomServiceTest {
 
   @BeforeAll
   public static void init() {
+    building1 = BuildingDTO.builder().buildingName("Fehérház").id(1L).city("Paks").build();
     meetingRoom1 = MeetingRoom.builder().id(1L).name("Kistárgyaló").numberOfSeats(12)
-        .projector(true).build();
+        .projector(true).building(building1.toEntity()).build();
     meetingRoom2 = MeetingRoom.builder().id(2L).name("Nagytárgyaló").numberOfSeats(40)
-        .projector(true).build();
+        .projector(true).building(building1.toEntity()).build();
     meetingRoomDTO1 = MeetingRoomDTO.builder().id(1L).name("Kistárgyaló").numberOfSeats(12)
-        .projector(true).build();
+        .projector(true).building(building1).build();
     meetingRoomDTO2 = MeetingRoomDTO.builder().id(2L).name("Nagytárgyaló").numberOfSeats(40)
-        .projector(true).build();
+        .projector(true).building(building1).build();
   }
 
   @Test
   public void findOneTest() {
     when(meetingRoomRepository.findById(1L)).thenReturn(Optional.of(meetingRoom1));
-    when(meetingRoomRepository.findById(3L)).thenThrow(new MeetingRoomNotFoundException());
-    assertThat(meetingRoomService.findOne(1L), is(Optional.of(meetingRoom1)));
+    assertThat(meetingRoomService.findOne(1L), is(Optional.of(meetingRoomDTO1)));
     verify(meetingRoomRepository, times(1)).findById(1L);
   }
 
   @Test
   public void findAllTest() {
-    List<MeetingRoom> expectedMeetingRoom = Arrays.asList(meetingRoom1, meetingRoom2);
+    List<MeetingRoom> expectedMeetingRoom = Arrays
+        .asList(meetingRoomDTO1.toEntity(), meetingRoomDTO2.toEntity());
     doReturn(expectedMeetingRoom).when(meetingRoomRepository).findAllByOrderById();
 
     List<MeetingRoomDTO> actualMeetingRoom = meetingRoomService.findAll();
-    assertEquals(actualMeetingRoom, expectedMeetingRoom);
+    assert (actualMeetingRoom.contains(meetingRoomDTO1));
 
   }
 
   @Test
   public void createTest() {
-    meetingRoomService.create(meetingRoomDTO1);
-    assertEquals("Kistárgyaló", meetingRoomDTO1.getName());
-    assertEquals(true, meetingRoom1.getProjector());
-    assertEquals(12, meetingRoomDTO1.getNumberOfSeats());
+    when(meetingRoomRepository.save(meetingRoomDTO1.toEntity())).thenReturn(meetingRoom1);
+    assertThat(meetingRoomService.create(meetingRoomDTO1), is(meetingRoomDTO1));
+    verify(meetingRoomRepository, times(1)).save(meetingRoomDTO1.toEntity());
   }
 
-  /*
- @Test
- public void updateMeetingRoomTest() {
-   meetingRoom = MeetingRoom.builder().name("Kistárgyaló").numberOfSeats(20).projector(true)
-       .build();
-   meetingRoomService.updateMeetingRoom(1L, meetingRoom);
-   assertEquals(1L, meetingRoom.getId());
-   assertEquals(meetingRoom.getId(), meetingRoom1.getId());
-   assertEquals(meetingRoom.getName(), meetingRoom1.getName());
-   assertEquals(meetingRoom.getProjector(), meetingRoom1.getProjector());
-   assertNotEquals(meetingRoom.getNumberOfSeats(), meetingRoom1.getNumberOfSeats());
- }
-*/
+  @Test
+  public void updateMeetingRoomTest() {
+    meetingRoom = MeetingRoom.builder().name("Kistárgyaló").numberOfSeats(20).projector(true)
+        .building(building1.toEntity())
+        .build();
+    meetingRoomService.updateMeetingRoom(1L, meetingRoomDTO1);
+    assertEquals(1L, meetingRoomDTO1.getId());
+    assertEquals(meetingRoomDTO1.getId(), meetingRoom1.getId());
+    assertEquals(meetingRoomDTO1.getName(), meetingRoom1.getName());
+    assertEquals(meetingRoomDTO1.getProjector(), meetingRoom1.getProjector());
+    assertNotEquals(meetingRoomDTO1.getNumberOfSeats(), meetingRoom1.getNumberOfSeats());
+  }
+
   @Test
   public void deleteMeetingRoomTest() {
     meetingRoomService.deleteMeetingRoom(1L);
