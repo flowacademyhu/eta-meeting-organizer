0072696de0a85454b33dca0cb6b693f62c304ce1
Tamas Vajgely <vajgi90@gmail.com>
2020-03-04 09:51:49 +0100

    Fixed query

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/MeetingRoomRepository.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/repository/MeetingRoomRepository.java
@@ -4,6 +4,7 @@ import hu.flowacademy.meetingorganizer.persistence.model.MeetingRoom;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,4 +18,7 @@ public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Long> 
   void deleteByIdIn(List<Long> id);
 
   List<MeetingRoom> findByBuilding_IdAndName(Long id, String name);
+
+  @Query("SELECT DISTINCT name FROM MeetingRoom m WHERE (Building.id = ?1)")
+  List<MeetingRoom> findByBuilding_Id(Long id);
 }
