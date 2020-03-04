834756334edf6fb108e38d93542372032225075e
Tamas Vajgely <vajgi90@gmail.com>
2020-03-04 01:34:33 +0100

    Initial commit of refresh

--- eta-meeting-organizer-frontend/src/app/calendar/components/calendar-header.component.ts
+++ eta-meeting-organizer-frontend/src/app/calendar/components/calendar-header.component.ts
@@ -2,13 +2,14 @@ import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, switchMap } from 'rxjs/operators';
 import { Building } from '~/app/models/building.model';
 import { MeetingRoom } from '~/app/models/meetingroom.model';
 import { Role } from '~/app/models/user.model';
 import { UserToken } from '~/app/shared/models/user-token.model';
 import { ApiCommunicationService } from '~/app/shared/services/api-communication.service';
 import { AuthService } from '~/app/shared/services/auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-calendar-header',
@@ -48,7 +49,7 @@ import { AuthService } from '~/app/shared/services/auth.service';
         </mat-form-field>
         <mat-form-field>
           <mat-label>{{'calendar-header.meeting-room' | translate}}</mat-label>
-            <mat-select formControlName="meetingRoom" [(ngModel)]="meetingRoom">
+            <mat-select (selectionChange)="setParam()" formControlName="meetingRoom" [(ngModel)]="meetingRoom">
               <mat-option>
                   <p align="center">--</p>
               </mat-option>
@@ -73,6 +74,7 @@ import { AuthService } from '~/app/shared/services/auth.service';
 export class CalendarHeaderComponent implements OnInit, OnDestroy {
   public checked: boolean = true;
   public cities: string[];
+  public city: string = '';
   public buildingId: number;
   public building: Building;
   public buildings: Building[];
@@ -81,6 +83,7 @@ export class CalendarHeaderComponent implements OnInit, OnDestroy {
   protected user: UserToken = {} as UserToken;
   protected isReader: boolean = false;
   private destroy$: Subject<boolean> = new Subject<boolean>();
+  private building$: Subject<void> = new Subject();
 
   public meetingRoomSelector: FormGroup = new FormGroup({
     building: new FormControl(),
@@ -90,12 +93,26 @@ export class CalendarHeaderComponent implements OnInit, OnDestroy {
 
   constructor(private readonly api: ApiCommunicationService,
               private changeDetectorRef: ChangeDetectorRef,
-              private readonly authService: AuthService) {
+              private readonly authService: AuthService,
+              private readonly router: Router,
+              private readonly route: ActivatedRoute) {
     this.authService.user.pipe(takeUntil(this.destroy$))
       .subscribe((data) => {
         this.user = data;
       });
     this.checkReader();
+    this.route.queryParams.pipe(takeUntil(this.destroy$))
+              .subscribe((params) => {
+                if (!params.ownReservation) {
+                  this.router.navigate(['calendar'], {
+                    queryParams: {
+                      ownReservation: true,
+                    },
+                });
+              }
+                this.checked = (params.ownReservation === 'true') ? true : false;
+                this.city = params['city'];
+              });
   }
 
   protected checkReader(): void {
@@ -106,38 +123,70 @@ export class CalendarHeaderComponent implements OnInit, OnDestroy {
     if (this.isReader) {
       this.checked = false;
       this.meetingRoomSelector.enable();
+    }
+    if (!this.checked) {
+      this.meetingRoomSelector.enable();
     } else {
       this.meetingRoomSelector.disable();
     }
+    this.meetingRoomSelector.valueChanges.subscribe(() => {
+      this.building$.next();
+    });
+    this.building$
+    .pipe(switchMap(() => {
+      return this.api.building()
+      .getCities();
+    }))
+    .subscribe((results) => {
+      this.cities = results;
+    });
     this.api
-      .building()
-      .getCities()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data) => {
-        this.cities = data;
-      });
+    .building()
+    .findByCity(this.city)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data) => {
+      this.buildings = data;
+    });
+    this.meetingRoomSelector.setValue(
+      city = this.city
+    )
   }
 
   public getBuildings() {
     this.api
-      .building()
-      .findByCity(this.meetingRoomSelector.controls.city.value)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data) => {
-        this.buildings = data;
-      });
+    .building()
+    .findByCity(this.meetingRoomSelector.controls.city.value)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data) => {
+      this.buildings = data;
+    });
+    this.router.navigate(['calendar'], {
+      queryParams: {
+        city: this.meetingRoomSelector.controls.city.value,
+      }, queryParamsHandling: 'merge',
+    });
   }
 
   public getMeetingrooms() {
     this.api
-      .meetingRoom()
-      .findByBuildingId(this.meetingRoomSelector.controls.building.value.id)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data) => {
-        this.meetingRooms = data;
-        this.changeDetectorRef.detectChanges();
-      });
+    .meetingRoom()
+    .findByBuildingId(this.meetingRoomSelector.controls.building.value.id)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data) => {
+      this.meetingRooms = data;
+      this.changeDetectorRef.detectChanges();
+    });
+    this.router.navigate(['calendar'], {
+      queryParams: { building: this.meetingRoomSelector.controls.building.value.id},
+       queryParamsHandling: 'merge' });
   }
+
+  public setParam() {
+    this.router.navigate(['calendar'], {
+      queryParams: {
+        meetingRoom: this.meetingRoomSelector.controls.meetingRoom.value.name
+      }, queryParamsHandling: 'merge'});
+    }
 
   public onCheck(event: MatCheckboxChange) {
     if (event.checked) {
@@ -147,6 +196,12 @@ export class CalendarHeaderComponent implements OnInit, OnDestroy {
       this.meetingRoomSelector.enable();
       this.checked = false;
     }
+    this.router.navigate(['calendar'], {
+      queryParams: {
+        ownReservation: this.checked,
+      },
+    });
+    // event.checked ? this.meetingRoomSelector.disable() : this.meetingRoomSelector.enable();
   }
 
   public ngOnDestroy(): void {
