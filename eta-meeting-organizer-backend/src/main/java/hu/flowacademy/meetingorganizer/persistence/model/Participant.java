63a02dcaa3ef45a14c71f744b1f7009e3e798bac
Ferenc Fabian <qwer.kocka@gmail.com>
2020-03-05 17:57:39 +0100

    I258 meetingroom service tests (#271)
    * create meetingroom service tests
    
    * remove orphan imports
    
    * modify findOne, findAll and creta tests
    
    * deleteAll and findByBuildingId methods tested
    
    * update test also works

--- eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Participant.java
+++ eta-meeting-organizer-backend/src/main/java/hu/flowacademy/meetingorganizer/persistence/model/Participant.java
@@ -1,13 +1,8 @@
 package hu.flowacademy.meetingorganizer.persistence.model;
 
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
